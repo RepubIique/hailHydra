e9a638c17d008ae939ed0746db6f7c42a5e5a704
Kendrick Bong <kendrickbong1996@gmail.com>
2023-08-01 21:41:34 +0800

    env

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -36,8 +36,8 @@ export const Contact = (props) => {
 
         emailjs
             .send(
-                process.env.VITE_APP_EMAILJS_SERVICE_ID,
-                process.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+                process.env.EMAILJS_SERVICE_ID,
+                process.env.EMAILJS_TEMPLATE_ID,
                 {
                     from_name: form.name,
                     to_name: 'HydraPower Engineering',
@@ -45,7 +45,7 @@ export const Contact = (props) => {
                     to_email: 'hydrapowerenginfo@gmail.com',
                     message: form.message,
                 },
-                process.env.VITE_APP_EMAILJS_PUBLIC_KEY
+                process.env.EMAILJS_PUBLIC_KEY
             )
             .then(
                 () => {
