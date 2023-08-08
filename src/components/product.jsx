851ef3927cd242890d79e5bd3eff0355d6ecce4f
Kendrick Bong <kendrickbong1996@gmail.com>
2023-08-08 21:16:53 +0800

    products boiler

---
+++ src/components/product.jsx
@@ -0,0 +1,11 @@
+import { Container } from 'react-bootstrap'
+
+export const Product = (props) => {
+    return (
+        <div id="product" className="text-center">
+            <Container>
+                <div></div>
+            </Container>
+        </div>
+    )
+}
