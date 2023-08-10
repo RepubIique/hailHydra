91a7279234959a51057ecc5d3102cfad7e94cf94
Kendrick Bong <kendrickbong1996@gmail.com>
2023-08-11 01:15:06 +0800

    optimised speed

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import { Container, Row, Col, Button } from 'react-bootstrap'
+import videoSourceMobile from '../assets/CNC-m.webm';
+import videoSourceDesktop from '../assets/CNC.webm'
 import '../styles/header.css'
 
 export const Header = (props) => {
+    // Determine which video source to use based on viewport width
+    const videoSource = window.innerWidth <= 600 ? videoSourceMobile : videoSourceDesktop;
+
     return (
         <header id="header">
             <div className="intro">
+                <video autoPlay muted loop>
+                    <source src={videoSource} type="video/webm" />
+                    Your browser does not support the video tag.
+                </video>
                 <div className="overlay">
                     <Container>
                         <Row className="justify-content-md-center">
@@ -15,14 +24,9 @@ export const Header = (props) => {
                                     <span></span>
                                 </h1>
                                 <p>
-                                    {props.data
-                                        ? props.data.paragraph
-                                        : 'Loading'}
+                                    {props.data ? props.data.paragraph : 'Loading'}
                                 </p>
-                                <Button
-                                    className="btn-lg button"
-                                    href="#contact"
-                                >
+                                <Button className="btn-lg button" href="#contact">
                                     Ask us how we can help
                                 </Button>
                             </Col>
@@ -31,5 +35,5 @@ export const Header = (props) => {
                 </div>
             </div>
         </header>
-    )
+    );
 }
